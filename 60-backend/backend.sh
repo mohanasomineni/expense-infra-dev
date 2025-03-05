ad7752256e5c7b357f5be01034abc1e99ded5f59
Mohanasomineni <mohanamanikanta7@gmail.com>
2025-03-05 13:55:39 -0500

    terraform

--- 60-backend/backend.sh
+++ 60-backend/backend.sh
@@ -5,4 +5,4 @@ dnf install ansible -y
 # ansible-playbook -i inventory mysql.yaml
 
 #PULL
-ansible-pull -i localhost -U https://github.com/mohanasomineni/expense-ansible-roles.git main.yaml -e COMPONENT=backend -e ENVIRONMENT=$1
+ansible-pull -i localhost, -U https://github.com/mohanasomineni/expense-ansible-roles-tf.git main.yaml -e COMPONENT=backend -e ENVIRONMENT=$1
